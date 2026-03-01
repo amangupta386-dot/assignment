9c9259dbc77b6a3c801817705ccccc5463a9f486
Aman Gupta <aman.gupta@walkingtree.tech>
2026-03-01 13:53:38 +0530

    delete all things

--- backend/routes/progressRoutes.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const auth = require("../middleware/authMiddleware");
-const { updateProgress, getProgress } = require("../controllers/progessController");
-
-router.get("/", auth, getProgress);
-router.post("/update", auth, updateProgress);
-
-module.exports = router;
